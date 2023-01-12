c4f605b38ea5a24ebf860e0e6a182587bde085d3
syo tokeshi <tokeshi@mfgnt.com>
2023-01-12 12:10:47 +0900

    削除

--- 7/cicket.rb
+++ 7/cicket.rb
@@ -1,6 +1,5 @@
 class Cicket
   attr_accessor :fare, :to, :stamped
-  # ENUM_TO = ["naha","tomi","itoman"]
 
   def initialize(fare,to)
     @fare = fare
@@ -12,7 +11,4 @@ class Cicket
     cicket.stamped = cicket.to
   end
 
-  def hoge
-
-  end
 end
